32d18ad5374b8574315753107675d1aa939f20bc
Maximilian Antoni <mail@maxantoni.de>
2012-11-04 21:37:40 +0100

    Timeouts for callbacks.

--- test/test-listener.js
+++ test/test-listener.js
@@ -9,6 +9,7 @@
 
 var test      = require('utest');
 var assert    = require('assert');
+var sinon     = require('sinon');
 
 var listen    = require('../lib/listen');
 
@@ -17,6 +18,11 @@ test('listener', {
 
   before: function () {
     this.listener = listen();
+    this.clock = sinon.useFakeTimers();
+  },
+
+  after: function () {
+    this.clock.restore();
   },
 
 
@@ -39,6 +45,79 @@ test('listener', {
 
     assert.equal('Error', error.name);
     assert.equal('Cannot be called after then', error.message);
+  },
+
+
+  'should err on timeout': function () {
+    var spy = sinon.spy();
+
+    this.listener(1000);
+    this.listener.then(spy);
+    this.clock.tick(999);
+
+    sinon.assert.notCalled(spy);
+
+    this.clock.tick(1);
+
+    sinon.assert.calledOnce(spy);
+    sinon.assert.calledWithMatch(spy, {
+      name : 'TimeoutError'
+    });
+  },
+
+
+  'should not set a timeout by default': function () {
+    var spy = sinon.spy();
+
+    this.listener();
+    this.listener.then(spy);
+    this.clock.tick(1);
+
+    sinon.assert.notCalled(spy);
+  },
+
+
+  'should not resolve if waiting for another callback': function () {
+    var spy = sinon.spy();
+
+    this.listener(1000);
+    this.listener();
+    this.listener.then(spy);
+    this.clock.tick(1000);
+
+    sinon.assert.notCalled(spy);
+  },
+
+
+  'should clear timeout': function () {
+    var spy       = sinon.spy();
+    var callback1 = this.listener(250);
+    var callback2 = this.listener();
+    this.listener.then(spy);
+
+    callback1();
+    this.clock.tick(250);
+    callback2();
+
+    sinon.assert.calledOnce(spy);
+    sinon.assert.calledWith(spy, null);
+  },
+
+
+  'should ignore callback arguments after timeout': function () {
+    var spy       = sinon.spy();
+    var callback1 = this.listener(500);
+    var callback2 = this.listener();
+    this.listener.then(spy);
+
+    this.clock.tick(500);
+    callback1(new TypeError());
+    callback2();
+
+    sinon.assert.calledOnce(spy);
+    sinon.assert.calledWithMatch(spy, {
+      name : 'TimeoutError'
+    });
   }
 
 
---
+++ test/test-timeout-error.js
@@ -0,0 +1,33 @@
+/**
+ * listen.js
+ *
+ * Copyright (c) 2012 Maximilian Antoni <mail@maxantoni.de>
+ *
+ * @license MIT
+ */
+'use strict';
+
+var test          = require('utest');
+var assert        = require('assert');
+
+var TimeoutError  = require('../lib/timeout-error');
+
+
+test('timeout-error', {
+
+
+  'should be error': function () {
+    var error = new TimeoutError();
+
+    assert(error instanceof Error);
+  },
+
+
+  'should be named TimeoutError': function () {
+    var error = new TimeoutError([]);
+
+    assert.equal(error.name, 'TimeoutError');
+  }
+
+
+});
